e41bb156e5c46ebe41d5023965fd40509624347b
tris84 <jollytris@gmail.com>
2020-02-04 23:02:08 +0900

    add firestore data

---
+++ app/src/main/java/com/tr1984/firebasesample/data/Poi.kt
@@ -0,0 +1,10 @@
+package com.tr1984.firebasesample.data
+
+import com.google.firebase.firestore.GeoPoint
+
+class Poi {
+
+    val point: GeoPoint = GeoPoint(0.0, 0.0)
+    val title: String = ""
+    val extras: List<String> = listOf()
+}
