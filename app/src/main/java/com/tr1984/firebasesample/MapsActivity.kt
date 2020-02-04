fe0315b8bab3dfa3d30b5f4fb60d65aeb8846ed4
tris84 <jollytris@gmail.com>
2020-02-04 23:28:07 +0900

    add initial remote config

--- app/src/main/java/com/tr1984/firebasesample/MapsActivity.kt
+++ app/src/main/java/com/tr1984/firebasesample/MapsActivity.kt
@@ -1,35 +1,49 @@
 package com.tr1984.firebasesample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.tr1984.firebasesample.extensions.disposeBag
+import com.tr1984.firebasesample.firebase.RemoteConfigHelper
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.PublishSubject
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var googleMap: GoogleMap
+    private var compositeDisposable = CompositeDisposable()
+    private var readySubject = PublishSubject.create<Unit>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+
+        readySubject.buffer(2)
+            .subscribe {
+                ready()
+            }.disposeBag(compositeDisposable)
+
+        RemoteConfigHelper.instance.fetchAndActivate {
+            readySubject.onNext(Unit)
+        }
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        this.googleMap = googleMap
+        readySubject.onNext(Unit)
 
 //        samplePoi.forEach {
 //            val latlng = LatLng(it.lat, it.lng)
@@ -37,6 +51,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 //            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 9f))
 //        }
     }
+
+    private fun ready() {
+        RemoteConfigHelper.instance.run {
+            val title = getString(RemoteConfigHelper.Key.MAIN_TITLE)
+            val dataSource = getString(RemoteConfigHelper.Key.DATA_SOURCE)
+            val lastUpdatedAt = getString(RemoteConfigHelper.Key.LAST_UPDATED_AT)
+            val contact = getString(RemoteConfigHelper.Key.CONTACT)
+            val poiMainTitle = getString(RemoteConfigHelper.Key.POI_MAIN_TITLE)
+
+            val initialMapPoint = get(RemoteConfigHelper.Key.INITIAL_MAP_POINT, HashMap::class.java)
+            val latitude = initialMapPoint["latitude"] as Double
+            val longitude = initialMapPoint["longitude"] as Double
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 9f))
+
+            Log.d("trtr", "title: $title, dataSource: $dataSource, lastUpdatedAt: $lastUpdatedAt, " +
+                    "contact: $contact, poiMainTitle: $poiMainTitle, latitude: $latitude, longitude: $longitude")
+        }
+    }
+
 
 //    private val samplePoi = arrayListOf(
 //        Poi(37.37478492230988, 126.72695961530327, "Baegot"),
