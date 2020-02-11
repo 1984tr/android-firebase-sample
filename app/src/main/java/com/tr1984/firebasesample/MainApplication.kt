66939f06c877a35bf9d1d6f323dd5b1b3e6d7263
tris84 <jollytris@gmail.com>
2020-02-11 22:43:27 +0900

    add push

--- app/src/main/java/com/tr1984/firebasesample/MainApplication.kt
+++ app/src/main/java/com/tr1984/firebasesample/MainApplication.kt
@@ -3,11 +3,14 @@ package com.tr1984.firebasesample
 import android.app.Application
 import com.tr1984.firebasesample.firebase.AnalyticsHelper
 import com.tr1984.firebasesample.firebase.CrashlyticsHelper
+import com.tr1984.firebasesample.util.Preferences
 
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Preferences.init(this)
 
         AnalyticsHelper.instance.run {
             setup(this@MainApplication)
--- app/src/main/java/com/tr1984/firebasesample/firebase/MapFirebaseMessagingService.kt
+++ app/src/main/java/com/tr1984/firebasesample/firebase/MapFirebaseMessagingService.kt
@@ -1,18 +1,25 @@
-package com.tr1984.firebasesample.service
+package com.tr1984.firebasesample.firebase
 
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.tr1984.firebasesample.util.Logger
+import com.tr1984.firebasesample.util.Preferences
 
 class MapFirebaseMessagingService : FirebaseMessagingService() {
 
-
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        Logger.d("onNewToken - token: $token")
+        Preferences.put(Preferences.Key.FcmToken, token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
+        Logger.d("onMessageReceived - ${remoteMessage.toString()}")
+        remoteMessage.notification?.run {
+
+        }
     }
 
     fun test() {
