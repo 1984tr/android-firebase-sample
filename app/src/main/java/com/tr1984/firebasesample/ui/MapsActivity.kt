a76b06c3a520b6e0a09586202c1feac172f1e9e0
tris84 <jollytris@gmail.com>
2020-02-15 21:29:08 +0900

    add naver map

--- app/src/main/java/com/tr1984/firebasesample/ui/MapsActivity.kt
+++ app/src/main/java/com/tr1984/firebasesample/ui/MapsActivity.kt
@@ -1,17 +1,18 @@
 package com.tr1984.firebasesample.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.tr1984.firebasesample.R
 import com.tr1984.firebasesample.databinding.ActivityMapsBinding
+import com.tr1984.firebasesample.extensions.alert
 import com.tr1984.firebasesample.extensions.disposeBag
 import com.tr1984.firebasesample.extensions.uiSubscribe
 import com.tr1984.firebasesample.firebase.AnalyticsHelper
-import com.tr1984.firebasesample.firebase.RemoteConfigHelper
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.BiFunction
 
 class MapsActivity : AppCompatActivity() {
 
@@ -26,43 +27,24 @@ class MapsActivity : AppCompatActivity() {
         AnalyticsHelper.instance.trackScreen(this)
 
         viewModel = MapsViewModel()
-        subscribes(viewModel)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         binding.viewModel = viewModel
         setContentView(binding.root)
 
-        val fm = supportFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-            ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map, it).commit()
-            }
-        mapFragment.getMapAsync(viewModel.mapReadyCallback)
-
-
-        mapFragment.getMapAsync { naverMap ->
-            this.naverMap = naverMap
-            settingMap()
-        }
-
-//        readySubject.buffer(2)
-//            .subscribe {
-//                ready()
-//            }.disposeBag(compositeDisposable)
-//
-//
-//
-//        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-//        mapFragment.getMapAsync { googleMap ->
-//            this@MapsActivity.googleMap = googleMap
-//            readySubject.onNext(Unit)
-//
-//            //        samplePoi.forEach {
-//            //            val latlng = LatLng(it.lat, it.lng)
-//            //            mMap.addMarker(MarkerOptions().position(latlng).title(it.title))
-//            //            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 9f))
-//            //        }
-//        }
+        Observable.combineLatest(
+            getMap(),
+            viewModel.getConfiguration(),
+            BiFunction<NaverMap, Unit, NaverMap> { map, _ ->
+                map
+            })
+            .uiSubscribe({
+                this.naverMap = it
+                settingMap()
+                viewModel.start()
+            }, {
+                this@MapsActivity.alert("알림", it.localizedMessage)
+            }).disposeBag(compositeDisposable)
     }
 
     override fun onDestroy() {
@@ -70,14 +52,18 @@ class MapsActivity : AppCompatActivity() {
         compositeDisposable.clear()
     }
 
-    private fun subscribes(viewModel: MapsViewModel) {
-        viewModel.readySubject
-            .buffer(2)
-            .uiSubscribe({
-
-            }, {
-
-            }).disposeBag(compositeDisposable)
+    private fun getMap(): Observable<NaverMap> {
+        return Observable.create<NaverMap> { emit ->
+            val fm = supportFragmentManager
+            val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+                ?: MapFragment.newInstance().also {
+                    fm.beginTransaction().add(R.id.map, it).commit()
+                }
+            mapFragment.getMapAsync { naverMap ->
+                emit.onNext(naverMap)
+                emit.onComplete()
+            }
+        }
     }
 
     private fun settingMap() {
@@ -85,30 +71,4 @@ class MapsActivity : AppCompatActivity() {
             isZoomControlEnabled = false
         }
     }
-
-    private fun ready() {
-        RemoteConfigHelper.instance.run {
-            val title = getString(RemoteConfigHelper.Key.MAIN_TITLE)
-            val dataSource = getString(RemoteConfigHelper.Key.DATA_SOURCE)
-            val lastUpdatedAt = getString(RemoteConfigHelper.Key.LAST_UPDATED_AT)
-            val contact = getString(RemoteConfigHelper.Key.CONTACT)
-            val poiMainTitle = getString(RemoteConfigHelper.Key.POI_MAIN_TITLE)
-
-            val initialMapPoint = get(RemoteConfigHelper.Key.INITIAL_MAP_POINT, HashMap::class.java)
-            val latitude = initialMapPoint["latitude"] as Double
-            val longitude = initialMapPoint["longitude"] as Double
-//            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 9f))
-
-            Log.d(
-                "trtr", "title: $title, dataSource: $dataSource, lastUpdatedAt: $lastUpdatedAt, " +
-                        "contact: $contact, poiMainTitle: $poiMainTitle, latitude: $latitude, longitude: $longitude"
-            )
-        }
-    }
-
-
-//    private val samplePoi = arrayListOf(
-//        Poi(37.37478492230988, 126.72695961530327, "Baegot"),
-//        Poi(37.41179071428517, 126.90895676504908, "SeokSu"),
-//        Poi(37.51224691265058, 127.11876347974098, "Bang-i"))
 }
--- app/src/main/java/com/tr1984/firebasesample/ui/MapsViewModel.kt
+++ app/src/main/java/com/tr1984/firebasesample/ui/MapsViewModel.kt
@@ -1,19 +1,23 @@
 package com.tr1984.firebasesample.ui
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
-import com.naver.maps.map.NaverMap
+import com.naver.maps.geometry.LatLng
 import com.tr1984.firebasesample.firebase.FirestoreHelper
 import com.tr1984.firebasesample.firebase.MapFirebaseMessagingService
 import com.tr1984.firebasesample.firebase.RemoteConfigHelper
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
 
 class MapsViewModel : ViewModel() {
 
-    val readySubject = PublishSubject.create<Unit>()
-    var mapReadyCallback = { naverMap: NaverMap ->
-
-    }
+    var positionSubject = PublishSubject.create<LatLng>()
+    var title = ObservableField("")
+    var dataSource = ObservableField("")
+    var lastUpdatedAt = ObservableField("")
+    var contact = ObservableField("")
+    var poiMainTitle = ObservableField("")
 
     private var compositeDisposable = CompositeDisposable()
 
@@ -22,13 +26,27 @@ class MapsViewModel : ViewModel() {
         compositeDisposable.clear()
     }
 
-    fun start() {
-        loadConfiguration()
+    fun getConfiguration() : Observable<Unit> {
+        return Observable.create<Unit> { emit ->
+            RemoteConfigHelper.instance.fetchAndActivate {
+                emit.onNext(Unit)
+                emit.onComplete()
+            }
+        }
     }
 
-    private fun loadConfiguration() {
-        RemoteConfigHelper.instance.fetchAndActivate {
-            readySubject.onNext(Unit)
+    fun start() {
+        RemoteConfigHelper.instance.run {
+            title.set(getString(RemoteConfigHelper.Key.MAIN_TITLE))
+            dataSource.set(getString(RemoteConfigHelper.Key.DATA_SOURCE))
+            lastUpdatedAt.set(getString(RemoteConfigHelper.Key.LAST_UPDATED_AT))
+            contact.set(getString(RemoteConfigHelper.Key.CONTACT))
+            poiMainTitle.set(getString(RemoteConfigHelper.Key.POI_MAIN_TITLE))
+
+            val initialMapPoint = get(RemoteConfigHelper.Key.INITIAL_MAP_POINT, HashMap::class.java)
+            val latitude = initialMapPoint["latitude"] as Double
+            val longitude = initialMapPoint["longitude"] as Double
+            positionSubject.onNext(LatLng(latitude, longitude))
         }
     }
 
