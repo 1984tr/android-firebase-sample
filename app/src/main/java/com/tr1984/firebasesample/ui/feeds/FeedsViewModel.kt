4a45ee703d7fa7deb50f31dc81d3d75c82ed1da6
tris84 <jollytris@gmail.com>
2020-03-03 23:51:41 +0900

    edit feed view model

--- app/src/main/java/com/tr1984/firebasesample/ui/feeds/FeedsViewModel.kt
+++ app/src/main/java/com/tr1984/firebasesample/ui/feeds/FeedsViewModel.kt
@@ -1,2 +1,17 @@
 package com.tr1984.firebasesample.ui.feeds
 
+import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+class FeedsViewModel : ViewModel() {
+
+    private var compositeDisposable = CompositeDisposable()
+
+    var items = ObservableArrayList<FeedViewModel>()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+}
