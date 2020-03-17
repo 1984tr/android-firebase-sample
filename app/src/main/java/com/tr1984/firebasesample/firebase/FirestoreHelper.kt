a57ad810ab1e0c7063f4094c835a252c077be8e2
tris84 <jollytris@gmail.com>
2020-03-17 23:28:04 +0900

    editing feed

--- app/src/main/java/com/tr1984/firebasesample/firebase/FirestoreHelper.kt
+++ app/src/main/java/com/tr1984/firebasesample/firebase/FirestoreHelper.kt
@@ -54,6 +54,52 @@ class FirestoreHelper private constructor() {
         }
     }
 
+ 
+
+//    fun getFeeds(callback: (List<Feed>) -> Unit) {
+//        firestore.collection("feeds").orderBy("time", Query.Direction.DESCENDING)
+//            .get().addOnSuccessListener { listener ->
+//                callback(listener.result.toObjects(Feed::class.java))
+//            }
+//    }
+//
+//    private fun getFeeds() : Single<QuerySnapshot> {
+//        return Single.create { emit ->
+//            firestore.collection("feeds")
+//                .get()
+//                .addOnSuccessListener {
+//                    it.documents.map {
+//                        it.toObject(Feed::class.java) ?: Feed()
+//                    }
+//                    emit.onSuccess(it)
+//                }
+//                .addOnFailureListener {
+//                    it.printStackTrace()
+//                    emit.onError(it)
+//                }
+//        }
+//    }
+//
+//    fun insertFeed(feed: Feed, callBack: (isSucess: Boolean) -> Unit) {
+//        val target = if (feed.id <= 0) {
+//            firestore.collection("feeds").document()
+//        } else {
+//            firestore.collection("feeds").document(feed.id.toString())
+//        }
+//
+//        feed.id = target.id
+//
+//        targetPost.set(post).addOnCompleteListener { result ->
+//            callBack(result.isSuccessful)
+//        }
+//    }
+//
+//    fun deleteFeed(id: String, completion: (success: Boolean) -> Unit) {
+//        firestore.collection("feeds").document(id).delete()
+//            .addOnCompleteListener { listener -> completion.invoke(listener.isSuccessful) }
+//            .addOnFailureListener { completion.invoke(false) }
+//    }
+
     companion object {
         val instance = FirestoreHelper()
     }
