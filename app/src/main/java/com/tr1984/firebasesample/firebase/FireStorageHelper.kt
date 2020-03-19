c92154a050bb652e72f161312657e40219f1d88b
tris84 <jollytris@gmail.com>
2020-03-19 22:49:28 +0900

    upload file

--- app/src/main/java/com/tr1984/firebasesample/firebase/FireStorageHelper.kt
+++ app/src/main/java/com/tr1984/firebasesample/firebase/FireStorageHelper.kt
@@ -7,19 +7,20 @@ import com.google.firebase.storage.StorageMetadata
 import io.reactivex.Single
 import java.io.File
 
-class FireStorageHelper private constructor() {
+object FireStorageHelper {
 
     private val storge by lazy {
         FirebaseStorage.getInstance("gs://fir-sample-aaf9e.appspot.com/")
     }
 
-    fun upload(path: String) : Single<String> {
+    fun upload(path: String): Single<String> {
         return Single.create<String> { emit ->
             val ref = storge.reference
-            val file = Uri.fromFile(File("path/to/mountains.jpg"))
+            val f = File(path)
+            val file = Uri.fromFile(f)
 
             val metadata = StorageMetadata.Builder()
-                .setContentType("image/jpeg")
+                .setContentType("image/${f.extension}")
                 .build()
 
             val uploadTask = ref.child("feeds/${file.lastPathSegment}").putFile(file, metadata)
--- app/src/main/java/com/tr1984/firebasesample/ui/feeds/FeedWriteActivity.kt
+++ app/src/main/java/com/tr1984/firebasesample/ui/feeds/FeedWriteActivity.kt
@@ -3,9 +3,15 @@ package com.tr1984.firebasesample.ui.feeds
 import android.app.Activity
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.tr1984.firebasesample.databinding.ActivityFeedWriteBinding
+import com.tr1984.firebasesample.extensions.uiSubscribeWithError
+import com.tr1984.firebasesample.firebase.FireStorageHelper
 
 class FeedWriteActivity : AppCompatActivity() {
 
@@ -32,6 +38,14 @@ class FeedWriteActivity : AppCompatActivity() {
                     val img = BitmapFactory.decodeStream(inputStream)
                     inputStream.close()
                     binding.attachmentImage.setImageBitmap(img)
+                    val path = getRealPathFromURI(it.data)
+                    Log.d("test", "path: $path")
+                    path?.run {
+                        FireStorageHelper.upload(this)
+                            .uiSubscribeWithError {
+                                Log.d("test", "complete: $it")
+                            }
+                    }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -49,5 +63,30 @@ class FeedWriteActivity : AppCompatActivity() {
             type = "image/*"
         }
         startActivityForResult(intent, 9001)
+    }
+
+    private fun getRealPathFromURI(contentUri: Uri): String? {
+        if (contentUri.path.startsWith("/storage")) {
+            return contentUri.path
+        }
+        val id = DocumentsContract.getDocumentId(contentUri).split(":")[1]
+        val columns = arrayOf(MediaStore.Files.FileColumns.DATA)
+        val selection = MediaStore.Files.FileColumns._ID + " = " + id
+        val cursor = contentResolver.query(
+            MediaStore.Files.getContentUri("external"),
+            columns,
+            selection,
+            null,
+            null
+        )
+        try {
+            val columnIndex = cursor.getColumnIndex(columns[0])
+            if (cursor.moveToFirst()) {
+                return cursor.getString(columnIndex)
+            }
+        } finally {
+            cursor.close()
+        }
+        return null
     }
 }
