c91a8b7f01a82fc00a78b90b2b22a76b821b59f2
tris84 <jollytris@gmail.com>
2020-04-02 00:20:01 +0900

    edit feed list

--- app/src/main/java/com/tr1984/firebasesample/extensions/ViewExtensions.kt
+++ app/src/main/java/com/tr1984/firebasesample/extensions/ViewExtensions.kt
@@ -1,6 +1,7 @@
 package com.tr1984.firebasesample.extensions
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableArrayList
 import androidx.recyclerview.widget.RecyclerView
@@ -20,5 +21,21 @@ fun View.visibleIfTrue(visible: Boolean) {
 fun RecyclerView.bindReplies(items: ObservableArrayList<ReplyViewModel>) {
     if (adapter is RepliesAdapter) {
         (adapter as RepliesAdapter).bind(items)
+    }
+}
+
+@BindingAdapter("loadImage")
+fun ImageView.load(url: String?) {
+    if (url != null) {
+        try {
+            if (this.context == null) {
+                return
+            }
+            Glide.with(this)
+                .load(url)
+                .into(this)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
