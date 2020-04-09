e7d2cd03ff8cf32063d5011da02d26ab0b707476
tris84 <jollytris@gmail.com>
2020-04-09 21:21:54 +0900

    add function

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.countReplyChange = functions.firestore
+    .document('feeds/{feedPath}/replies/{replyPath}')
+    .onCreate((snap, context) => {
+        // Get an object representing the document
+        // e.g. {'name': 'Marie', 'age': 66}
+        const newValue = snap.data();
+        console.log(newValue)
+
+        // perform desired operations ...
+    });
