e9832cba77a330abae6c02462f44acc940fe39c6
tris84 <jollytris@gmail.com>
2020-04-13 23:58:42 +0900

    edit functions

--- functions/index.js
+++ functions/index.js
@@ -12,11 +12,10 @@ const db = admin.firestore();
 //  response.send("Hello from Firebase!");
 // });
 
-const replyRef = functions.firestore.document('feeds/{feedPath}/replies/{replyPath}')
-exports.countReplyChange = replyRef.onCreate((snap, context) => {
+const createRef = functions.firestore.document('feeds/{feedPath}/replies/{replyPath}')
+exports.createReply = createRef.onCreate((snap, context) => {
     
     let feedPath = snap.ref.parent.parent.path
-        
     let ref = db.doc(feedPath)
     let feedDoc = ref.get()
         .then(doc => {
@@ -24,8 +23,35 @@ exports.countReplyChange = replyRef.onCreate((snap, context) => {
                 console.log('No such document!');
             } else {
                 console.log('Document data:', doc.data());
+
+                let setWithOptions = ref.set({
+                    replyCount: doc.data().replyCount + 1
+                }, {merge: true});
             }
-            return doc.data()
+            return;
+        })
+        .catch(err => {
+            console.log('Error getting document', err);
+        });
+});
+
+const deleteRef = functions.firestore.document('feeds/{feedPath}/replies/{replyPath}')
+exports.deleteReply = deleteRef.onDelete((snap, context) => {
+      
+    let feedPath = snap.ref.parent.parent.path
+    let ref = db.doc(feedPath)
+    let feedDoc = ref.get()
+        .then(doc => {
+            if (!doc.exists) {
+                console.log('No such document!');
+            } else {
+                console.log('Document data:', doc.data());
+
+                let setWithOptions = ref.set({
+                    replyCount: doc.data().replyCount - 1
+                }, {merge: true});
+            }
+            return;
         })
         .catch(err => {
             console.log('Error getting document', err);
