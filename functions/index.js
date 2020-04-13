a37c9f68bc6a0d1b16e19efeae7e3a1aebb59829
tris84 <jollytris@gmail.com>
2020-04-13 23:20:45 +0900

    editing function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,13 +12,22 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-exports.countReplyChange = functions.firestore
-    .document('feeds/{feedPath}/replies/{replyPath}')
-    .onCreate((snap, context) => {
-        // Get an object representing the document
-        // e.g. {'name': 'Marie', 'age': 66}
-        const newValue = snap.data();
-        console.log(newValue)
-
-        // perform desired operations ...
-    });
+const replyRef = functions.firestore.document('feeds/{feedPath}/replies/{replyPath}')
+exports.countReplyChange = replyRef.onCreate((snap, context) => {
+    
+    let feedPath = snap.ref.parent.parent.path
+        
+    let ref = db.doc(feedPath)
+    let feedDoc = ref.get()
+        .then(doc => {
+            if (!doc.exists) {
+                console.log('No such document!');
+            } else {
+                console.log('Document data:', doc.data());
+            }
+            return doc.data()
+        })
+        .catch(err => {
+            console.log('Error getting document', err);
+        });
+});
